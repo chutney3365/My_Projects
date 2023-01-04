22d71ffc906a88de64d007bcd0cad264de672da2
sd6040 <sd60@illinois.edu>
2023-01-03 19:29:55 -0600

    cleaned up caesar shift code

--- Python/CaesarCipherShift.py
+++ Python/CaesarCipherShift.py
@@ -1,45 +1,27 @@
-# could use interface?
-# does not handle special characters
+# could use interface? PySimpleGUI; tkinter
+# does not handle special characters as of now
+# todo: let user input their list of letters in their order
 
-def caesar_cipher_shift(shift_direction, shift_num):
-    # Returns dictionary
+def caesar_cipher_shift(shift_num):
+    # Returns keyed dictionary
     letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     shifted_letters = dict()
     for i in range (len(letters)):
-        if (shift_direction == "right"):
-            shifted_letters[letters[i]] = letters[(i + shift_num) % len(letters)]
-        else:
-            shifted_letters[letters[i]] = letters[(26 + i - shift_num) % len(letters)]
+        shifted_letters[letters[i]] = letters[(i + shift_num) % len(letters)]
     return shifted_letters
-
 
 def caesar_cipher_encode(msg, key):
     coded_msg = ""
     for letter in msg:
-        if (letter.isalpha()):
-            coded_msg += key[letter]
-        else:
-            coded_msg += letter
+        coded_msg += key.get(letter, letter)
     return coded_msg
-
-def caesar_cipher_decode(coded_msg, key):
-    decoded_msg = ""
-    for letter in coded_msg:
-        if (letter.isalpha()):
-            char = list(key.keys())[list(key.values()).index(letter)] # what is this doing???
-            decoded_msg += char
-        else:
-            decoded_msg += letter
-    return decoded_msg
 
 if __name__ == "__main__":
     process = input("Do you want to encode or decode your message? ").lower()
     msg = input("Type you message: ")
-    shift_direction = input("Enter Caeser shift direction (right/left): ").lower()
-    shift_num = int(input("Enter number of places to shift by: "))
-    key = caesar_cipher_shift(shift_direction, shift_num)
-    if (process == "encode"):
-        output = caesar_cipher_encode(msg, key)
-    else:
-        output = caesar_cipher_decode(msg, key)
+    shift_num = int(input("Enter key: "))
+    if (process == "decode"):
+        shift_num *= -1
+    key_dict = caesar_cipher_shift(shift_num)
+    output = caesar_cipher_encode(msg, key_dict)
     print(output)
